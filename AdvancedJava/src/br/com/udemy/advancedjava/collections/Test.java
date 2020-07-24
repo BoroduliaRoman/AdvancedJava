de4cd2c2ed897de73e2f160817af3a92ef93fd0b
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-07-24 20:03:31 +0300

    Add files via upload

---
+++ AdvancedJava/src/br/com/udemy/advancedjava/collections/Test.java
@@ -0,0 +1,29 @@
+package br.com.udemy.advancedjava.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			list.add(i);
+		}
+
+//		System.out.println(arrayList);//		
+//		System.out.println(arrayList.get(0));
+//		System.out.println(arrayList.get(99));//		
+//		System.out.println(arrayList.size());			
+//		for(int i = 0; i < arrayList.size(); i++) {
+//			System.out.println(arrayList.get(i));
+//		}
+//		
+//		for(Integer x : arrayList) {
+//			System.out.println(x);
+//		}
+		
+		list.remove(0);
+		System.out.println(list);
+	}
+}
