a1ce50c93645d85cec66904e6e204593f73610ac
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-07-25 12:04:35 +0300

    Add files via upload

---
+++ AdvancedJava/src/br/com/udemy/advancedjava/collections/lesson3/Test.java
@@ -0,0 +1,11 @@
+package br.com.udemy.advancedjava.collections.lesson3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>(1000);	
+	}
+}
---
+++ AdvancedJava/src/br/com/udemy/advancedjava/collections/lesson4/Test.java
@@ -0,0 +1,34 @@
+package br.com.udemy.advancedjava.collections.lesson4;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<Integer> linkedList = new LinkedList<>();
+		
+//		linkedList.add(1);
+//		linkedList.get(0);
+//		linkedList.size();
+//		linkedList.remove(0);
+		
+		List<Integer> arrayList = new ArrayList<>();
+		
+		measureTime(linkedList);
+		measureTime(arrayList);
+	}
+	
+	private static void measureTime(List<Integer> list) {
+		long start = System.currentTimeMillis();
+		
+		for(int i = 0; i < 1000000; i++ ) {
+			list.add(i);
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println(end - start);
+	}
+}
