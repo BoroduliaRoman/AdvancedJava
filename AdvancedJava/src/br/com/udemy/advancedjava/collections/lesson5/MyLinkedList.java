d0f452ff56c1c14f0c6b118b4c2a513672da122f
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-07-27 16:29:42 +0300

    Add files via upload

---
+++ AdvancedJava/src/br/com/udemy/advancedjava/collections/lesson5/MyLinkedList.java
@@ -0,0 +1,117 @@
+package br.com.udemy.advancedjava.collections.lesson5;
+
+import java.util.Arrays;
+
+public class MyLinkedList {
+	private Node head;
+	private int size;
+
+	public void add(int value) {
+		// если это первое добавление в список
+		if (head == null) {
+			// то здесь будет какая-то логика
+			this.head = new Node(value);
+		} else {
+			Node temp = head;
+
+			while (temp.getNext() != null) {
+				temp = temp.getNext();
+			}
+
+			temp.setNext(new Node(value));
+		}
+
+		size++;
+	}
+
+	public int get(int index) {
+		int currentIndex = 0;
+		Node temp = head;
+
+		while (temp != null) {
+			if (currentIndex == index) {
+				return temp.getValue();
+			} else {
+				temp = temp.getNext();
+				currentIndex++;
+			}
+		}
+
+		throw new IllegalArgumentException();
+	}
+
+	public void remove(int index) {
+		if (index == 0) {
+			head = head.getNext();
+			size--;
+			return;
+		}
+
+		int currentIndex = 0;
+		Node temp = head;
+
+		while (temp != null) {
+			if (currentIndex == index - 1) {
+				temp.setNext(temp.getNext().getNext());
+				size--;
+				return;
+			} else {
+				temp = temp.getNext();
+				currentIndex++;
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		int[] result = new int[size];
+
+		int idx = 0;
+		Node temp = head;
+
+		while (temp != null) {
+			result[idx++] = temp.getValue();
+			temp = temp.getNext();
+		}
+
+		return Arrays.toString(result);
+	}
+
+	private static class Node {
+		private int value;
+		private Node next;
+
+		public Node(int value) {
+			super();
+			this.value = value;
+		}
+
+		/**
+		 * @return the value
+		 */
+		public int getValue() {
+			return value;
+		}
+
+		/**
+		 * @param value the value to set
+		 */
+		public void setValue(int value) {
+			this.value = value;
+		}
+
+		/**
+		 * @return the next
+		 */
+		public Node getNext() {
+			return next;
+		}
+
+		/**
+		 * @param next the next to set
+		 */
+		public void setNext(Node next) {
+			this.next = next;
+		}
+	}
+}
