a8c5f69a1c9040c325ae33e3a45151502a371bc1
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-08-03 16:03:34 +0300

    Add files via upload

---
+++ AdvancedJava/src/br/com/udemy/advancedjava/multithreading/lesson27/Test.java
@@ -0,0 +1,90 @@
+package br.com.udemy.advancedjava.multithreading.lesson27;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+
+	public static void main(String[] args) throws InterruptedException {
+//		Semaphore semaphore = new Semaphore(3);
+//
+//		try {
+//			semaphore.acquire();
+//			semaphore.acquire();
+//			semaphore.acquire();
+//
+//			System.out.println("All permits have been acuired");
+//
+//			semaphore.acquire();
+//			
+//			System.out.println("Can't reach here...");
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		semaphore.release();
+//
+//		System.out.println(semaphore.availablePermits());
+
+		ExecutorService executorService = Executors.newFixedThreadPool(200);
+
+		Connection connection = Connection.getConnection();
+		for (int i = 0; i < 10; i++) {
+			executorService.submit(new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						connection.work();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+			});
+		}
+		
+		executorService.shutdown();
+		executorService.awaitTermination(1, TimeUnit.DAYS);
+	}
+
+}
+
+class Connection {
+	private static Connection connection = new Connection();
+	private int connectionsCount;
+	private Semaphore semaphore = new Semaphore(10);
+
+	private Connection() {
+
+	}
+
+	public static Connection getConnection() {
+		return connection;
+
+	}
+	
+	public void work() throws InterruptedException {
+		semaphore.acquire();
+		try {
+			doWork();	
+		} finally {
+			semaphore.release();			
+		}
+	}
+
+	private void doWork() throws InterruptedException {
+		synchronized (this) {
+			connectionsCount++;
+			System.out.println(connectionsCount);
+		}
+
+		Thread.sleep(5000);
+
+		synchronized (this) {
+			connectionsCount--;
+		}
+	}
+
+}
