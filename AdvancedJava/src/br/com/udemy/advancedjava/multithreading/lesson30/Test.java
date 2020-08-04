d0d8c39ab8c369ab3e8365935cb40f25ffa36264
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-08-04 10:55:45 +0300

    Add files via upload

---
+++ AdvancedJava/src/br/com/udemy/advancedjava/multithreading/lesson30/Test.java
@@ -0,0 +1,42 @@
+package br.com.udemy.advancedjava.multithreading.lesson30;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+		Future<Integer> future = executorService.submit(() -> {
+			System.out.println("Starting");
+			Thread.sleep(500);
+			System.out.println("Finished");
+			
+			Random random = new Random();
+			int randomValue = random.nextInt();
+			
+			if (randomValue < 5) {
+				throw new Exception("Something bad happening");
+			}
+			
+			return random.nextInt(10);
+
+		});
+
+		executorService.shutdown();
+		
+		try {
+			int result = future.get();
+			System.out.println(result);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			Throwable ex = e.getCause();
+			System.out.println(ex.getMessage());
+		}
+	}
+}
